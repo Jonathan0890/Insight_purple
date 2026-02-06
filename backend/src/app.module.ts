32bf3b5aee028fd18fb8b8e29e9890d58233c3ff
Jonathan0890 <jonathan.coox15@gmail.com>
2026-02-06 12:58:08 -0500

    Implementacion de dependencias

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthsModule } from './auths/auths.module';
+import { IntegrationsModule } from './integrations/integrations.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { MetricsModule } from './metrics/metrics.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthsModule, IntegrationsModule, DashboardModule, MetricsModule],
   controllers: [AppController],
   providers: [AppService],
 })
